2db8e7bb756b9886454c1bf4b18c9ed4a81c277e
andrei-peterfi <peterfi.andrei@gmail.com>
2025-02-28 22:55:04 +0200

    v5

--- _04Python_Playwright/tests/_01AndreiPeterfi/e2e/form_authentication.py
+++ _04Python_Playwright/tests/_01AndreiPeterfi/e2e/form_authentication.py
@@ -1,16 +1,24 @@
+from tests._01AndreiPeterfi.pages.form_authentication_page import FormAuthenticationPage
 from tests._01AndreiPeterfi.pages.home_page import HomePage
 
 
-class TestHome:
-    def test_home_page_title(self, page):
+class TestFormAuthentication:
+    def test_form_authentication_page_title(self, page):
         homePage = HomePage(page)
         homePage.goto("https://the-internet.herokuapp.com/")
+        homePage.navigate_to_page("Form Authentication")
         assert page.title() == "The Internet"
 
-    def test_no_of_list_elements(self, page):
+    def test_succesfull_login(self, page):
         homePage = HomePage(page)
         homePage.goto("https://the-internet.herokuapp.com/")
-        assert homePage.getNoOfListElements() == 44
+        homePage.navigate_to_page("Form Authentication")
+        formAuthenticationPage = FormAuthenticationPage(page)
+        formAuthenticationPage.setUsernameAndPassword("tomsmith", "SuperSecretPassword!")
+
+
+
+
 
     def test_navigate_to_another_page(self, page):
         homePage = HomePage(page)
